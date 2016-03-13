ccb935826a6053dadb2c1e5069f61a7596782788
Emil Persson <emil.persson@creuna.se>
2016-03-13 22:28:37 +0100

    Stub out build system for api and docs

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Build site
 pub build
+sh docs/build.sh
+sh api/build.sh
+
+# Commit and push subtree
 git add build/web
 git commit -m "Build $(date -u +"%F %T %Z")"
 git subtree push --prefix build/web origin master
---
+++ docs/build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Build docs to build/web/docs
